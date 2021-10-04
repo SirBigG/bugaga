cc8b0e64d33e5c11fd204401550cd96bb98752e9
SirBigG <gotsandriy@gmail.com>
2021-10-04 22:55:59 +0300

    Added mode

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from settings import settings
 
 from models.auth import User
 
-
 bot = Bot(api_token=settings.TELEGRAM_TOKEN)
 
 session = Session()
@@ -19,11 +18,14 @@ async def start(chat: Chat, match):
         user = User(telegram_key=chat.id)
     session.add(user)
     session.commit()
-    await chat.send_text("Дякуємо! Ви успішно підписались на агроновини! "
-                         "Як тільки з'являться публікації вони відразу приходитимуть Вам. "
-                         "Для того щоб запропонувати додати Ваш улюблений ресурс до списку відслідковування, "
-                         "зв'яжіться з нами через форму "
-                         "<a href='https://agromega.in.ua/feedback/'>зворотнього зв'язку</a>.")
+    await chat.send_text(
+        "Дякуємо! Ви успішно підписались на агроновини! "
+        "Як тільки з'являться публікації вони відразу приходитимуть Вам. "
+        "Для того щоб запропонувати додати Ваш улюблений ресурс до списку відслідковування, "
+        "зв'яжіться з нами через форму "
+        "<a href='https://agromega.in.ua/feedback/'>зворотнього зв'язку</a>.",
+        parse_mode="HTML"
+    )
 
 
 if __name__ == "__main__":
